e7ea3b9da44758a64b31ca17867c2c0130f27790
mano jyoti <manojyoti434@gmail.com>
2025-07-13 15:53:12 +0530

    multiple-env-tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID of the EC2 instance"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+    description = "Instance size"
+
+    validation {
+        condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+        error_message = "Valid values for instance_type are: t3.micro, t3.small, t3.medium"
+    } 
+}
+
+# mandatory to provide according to their project
+variable "sg_ids" {
+    type = list
+}
+
+variable "tags" {
+    type = map
+}
+
